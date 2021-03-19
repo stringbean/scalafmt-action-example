8e24d0ee12d1c678f9af0e95a2bb860b46dfb6f8
Michael Stringer <stringbean@users.noreply.github.com>
2021-03-19 09:26:35 +0000

    Initial project

---
+++ src/test/scala/software/purpledragon/scalafmtexample/ScalafmtExampleSpec.scala
@@ -0,0 +1,13 @@
+package software.purpledragon.scalafmtexample
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class ScalafmtExampleSpec extends AnyFlatSpec with Matchers {
+  val scalafmtExample = new ScalafmtExample()
+
+  "greet" should "greet bob" in {
+    scalafmtExample.greet("bob") shouldBe "hello, bob"
+  }
+
+}
